f5694487b1b8ce7830ff2bd607b72d7bc2a898c9
John <theminor@users.noreply.github.com>
2018-02-12 17:57:39 -0500

    Update node-august-home.js

--- node-august-home.js
+++ node-august-home.js
@@ -1,71 +1,20 @@
 var request = require('request');
-var hdrs = require('./requestHeaders');
-
-// status codes that apparently get returned from successful web requests
-const ConnectSuccess = 200;
-const PostSuccess = 302;
-const SuccessStatusMsg = 'OK';
-
-var tccRequest = function(node, headers, debugIdentifier, successStatusCode, callback) {
-	request(headers, function(err, response) {
-		node.statusTxt = debugIdentifier + ': ';
-		if (response) {
-			node.statusTxt += 'Response: ' + JSON.stringify(response);
-			if (response.statusCode) node.statusCode = response.statusCode;
-			if (response.body) {
-				try {
-					node.statusData = JSON.parse(response.body);
-				} catch(pErr) {
-					node.statusTxt = ' -- with error parsing JSON Response Body: ' + pErr;
-					node.statusData = response.body;                            			// if body can't be parsed as JSON, retun the body as-is
-				}
-			}
-		}
-		if (err || response.statusCode != successStatusCode || (response && response.statusMessage) ? response.statusMessage != SuccessStatusMsg : false) {	// possible issue: response.statusMessage may not always be "OK" for all requests? Need to verify
-			node.connected = false;
-			node.statusTxt += 'Error: ' + err;
-			return node;
-		} else {
-			node.statusTxt += ' -- success';
-			node.connected = true;
-			callback(node)
-		}
-	});
-};
-
-var tccLogin = function(node, callback) {
-	tccRequest(node, hdrs.getDefaults(node), 'TCC Login first GET', ConnectSuccess, function(node) {
-		tccRequest(node, hdrs.postDefaults(node), 'TCC Login POST', PostSuccess, function(node) {
-			tccRequest(node, hdrs.getDefaults(node), 'TCC Login second GET', ConnectSuccess, function(node) {
-				callback(node);
-			});
-		});
-	});
-};
+var reqData = require('./requestData.js');
 
 module.exports = function(RED) {
-	RED.nodes.registerType('tcc-honeywell', function(config) {
+	RED.nodes.registerType('august-home', function(config) {
 		RED.nodes.createNode(this, config);
 		var node = this;
-		node.jar = request.jar();
-		node.connected = false;
 		node.on('input', function(msg) {
-			var sendMsg = function(node) {
-				msg.payload = node.statusData;
-				msg.title = 'Honeywell TCC Data';                                           // see https://github.com/node-red/node-red/wiki/Node-msg-Conventions
-				msg.description = 'JSON data from Honeywell TCC';
-				msg.tccStatusTxt = node.statusTxt;											// useful for debugging
+			var sendMsg = function(sendDta) {
+				msg.payload = sendDta;
+				msg.title = 'August Data';                                           // see https://github.com/node-red/node-red/wiki/Node-msg-Conventions
+				msg.description = 'Response data from August API';
 				node.send(msg);
 			};
-            var process = function() {
-                if (typeof msg.payload !== 'string') msg.payload = JSON.stringify(msg.payload);
-                if (msg.payload.charAt(0) === '{') {										// it is a command
-                    tccRequest(node, hdrs.changeSetting(node, msg.payload), 'TCC Command ' + msg.payload + ': ', ConnectSuccess, sendMsg);
-                } else {																	// it is a status request
-                    tccRequest(node, hdrs.getStatus(node), 'TCC Status GET', ConnectSuccess, sendMsg);
-                }
-            };
-            if (node.connected) process(); else tccLogin(node, setTimeout(process, 1000));	// added a delay to see if needed (?)
+			
+			// *** TO-DO
+
 		});
-	}, { credentials: { username: {type: "text"}, password: {type: "password"},	deviceID: {type: "text"} } } );
+	}, { credentials: { email: {type: "text"}, phone: {type: "text"}, apiKey: {type: "password"} } } );
 };
