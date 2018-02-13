c273fbe70409cd352f4cf896732707e033a8fa0d
John <theminor@users.noreply.github.com>
2018-02-13 09:22:44 -0500

    first function: getFirmwareKeys()

--- requestData.js
+++ requestData.js
@@ -1,30 +1,33 @@
-
-async function getFirmwareKeys() {
-  try{
-    var locks = await api.getLocks();
-    var lockIds = Object.keys(locks);
-
-    for (var i = 0; i < lockIds.length; i++) {
-      var l = locks[lockIds[i]];
-      try {
-        var firmware = await api.getTiFirmware(lockIds[i], '1.1.20');
-        var extraDataStart = firmware.length - 68;
-        var serialNumber = firmware.toString('ascii', extraDataStart, extraDataStart + 10);
-        var readLockId = firmware.toString('hex', extraDataStart + 16, extraDataStart + 32);
-        var firmwareKey = firmware.toString('hex', extraDataStart + 48, extraDataStart + 64);
-		  
-		// *** TO DO - deal with data, don't log
-        console.log(l);
-        console.log('sn: '+ serialNumber);
-        console.log('id: '+ readLockId);
-        console.log('key: '+ firmwareKey);
-      } catch(e){
-        console.log("Could not get key for " + l)
-      }
-    }
-  } catch(e) {
-    console.log(e);
-  }
-}
-
-getFirmwareKeys();
+module.exports = {
+	
+	// *** TO DO - add description
+	var getFirmwareKeys = async function(api, node) {
+		try {
+			var locks = await api.getLocks();
+			var lockIds = Object.keys(locks);
+			var rtn = [];
+			for (var i = 0; i < lockIds.length; i++) {
+				var l = locks[lockIds[i]];
+				try {
+					var firmware = await api.getTiFirmware(lockIds[i], '1.1.20');
+					var extraDataStart = firmware.length - 68;
+					var serialNumber = firmware.toString('ascii', extraDataStart, extraDataStart + 10);
+					var readLockId = firmware.toString('hex', extraDataStart + 16, extraDataStart + 32);
+					var firmwareKey = firmware.toString('hex', extraDataStart + 48, extraDataStart + 64);
+					rtn.push({
+						"l": l,
+						"sn": serialNumber,
+						"id": readLockId,
+						"key": firmwareKey
+					});
+				} catch(e){
+					node.warn('Error in getFirmwareKeys(): ' + e + ' -- Could not get key for ' + l);
+				}
+			}
+			if (rtn.length) return rtn; else return null; 
+		} catch(e) {
+			node.warn('Error in getFirmwareKeys(): ' + e);
+			return null;
+		}
+	}
+};
