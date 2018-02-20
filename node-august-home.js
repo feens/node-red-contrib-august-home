f30661523700eb023aa1f6b5d0ccba66ba36b653
theminor <jminor@mbnlaw.mbnlaw.com>
2018-02-20 16:26:37 -0500

    add credentials; add options

--- node-august-home.js
+++ node-august-home.js
@@ -1,18 +1,28 @@
-var request = require('request');
 const Api = require('./api.js');
-var reqData = require('./requestData.js');
+const reqData = require('./requestData.js');
 
 module.exports = function(RED) {
-	RED.nodes.registerType('august-home', function(config) {
-		RED.nodes.createNode(this, config);
-		var node = this;		
-		var apiDta = {
+	RED.nodes.registerType('august-home', function(nodeConfig) {
+		RED.nodes.createNode(this, nodeConfig);
+		let node = this;		
+		let config = {									// configs used in various functions - many use different aliases, so all are included, rather than re-writing all of jmaxxz's code
 			"jwt": node.credentials.apiKey,				// The x-august-access-token - Obtained via the authentication apis (see https://github.com/jmaxxz/keymaker/issues/5#issuecomment-360007335)
-			"installId": node.credentials.installId,	// A random value used by the APIs to determine if this device is authorized to access this user's account.
+			"x-august-access-token": node.credentials.apiKey,
+			"apiKey": node.credentials.apiKey,
+			"ApiKey": node.credentials.apiKey,
 			"keaseApiKey": "14445b6a2dba",				// Constant. An key which appears to be shared by all users which provides API access. This doesn't change and we always use the provided value.
+			"x-kease-api-key": "14445b6a2dba",
+			"x-august-api-key": "14445b6a2dba",
+			"KeaseApiKey": "14445b6a2dba",
+			"installId": node.credentials.installId,	// A random value used by the APIs to determine if this device is authorized to access this user's account.
+			"content-type": "application/json",
+			"Email": node.credentials.email,
+			"Phone": node.credentials.phone,
+			"Password": node.credentials.password
 		};
+		// other options used in requests.json: {{ListingId}} {{LockId}} {{MfgBridgeId}} {{BridgeId}} {{NestcamId}} {{SSID}} {{WiFiPSK}} {{BridgeAuthToken}} {{HouseId}} {{DoorbellId}} {{DoorbellSn}} {{GuestUserId}} {{UserType}} {{RuleId}} {{KeypadId}} {{KeypadSn}} {{UserId}} {{action}} {{PartnerId}}
 
-		var api = new Api(apiDta);		
+		var api = new Api(config);		
 		
 		node.on('input', function(msg) {
 			var sendMsg = function(sendDta) {
@@ -25,5 +35,5 @@ module.exports = function(RED) {
 			// *** TO-DO
 
 		});
-	}, { credentials: { email: {type: "text"}, phone: {type: "text"}, installId: {type: "text"}, apiKey: {type: "password"} } } );
+	}, { credentials: { email: {type: "text"}, phone: {type: "text"}, installId: {type: "text"}, password: {type: "password"}, apiKey: {type: "password"} } } );
 };
